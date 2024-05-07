fbbce7736d95534005b774ad919c9c3d23ea3ba4
Leandro Heffes <leandroheffes@outlook.es>
2024-05-07 10:57:40 -0300

    banner section for mobile ready and header for mobile in progress

---
+++ public/js/headerNavbarScript.js
@@ -0,0 +1,6 @@
+const headerNavbar = document.querySelector(".headerNavbar")
+const headerOpenNavbarButton = document.querySelector(".headerOpenNavbarButton")
+
+headerOpenNavbarButton.addEventListener("click", () => {
+    headerNavbar.style.display = "block"
+})
