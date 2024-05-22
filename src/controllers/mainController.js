7b576806ccfb9bc28c07bc5221a98b6fbc864ac1
Leandro Heffes <leandroheffes@outlook.es>
2024-05-22 11:39:38 -0300

    register page for mobile complete

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,5 +3,9 @@ const categoriesServices = require("../services/categoriesServices")
 module.exports = {
     index: (req, res) => {
         return categoriesServices.getAllCategories().then(categories => res.render("index", {categories}))
+    },
+
+    register: (req, res) => {
+        return res.render("register")
     }
 }
--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -3,5 +3,6 @@ const mainController = require("../controllers/mainController")
 const router = Router()
 
 router.get("/", mainController.index)
+router.get("/register", mainController.register)
 
 module.exports = router
