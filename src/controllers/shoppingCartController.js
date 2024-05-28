b83190ecfadb23ffa8e3b1b2ac017fcce7434e5b
Leandro Heffes <leandroheffes@outlook.es>
2024-05-28 11:27:36 -0300

    shopping cart in progress till tablet view and delete button is not ready

--- src/controllers/shoppingCartController.js
+++ src/controllers/shoppingCartController.js
@@ -1,11 +1,26 @@
 const shoppingCartServices = require("../services/shoppingCartServices")
+const productsServices = require("../services/productsServices")
 
 module.exports = {
-    shoppingCart: (req, res) => {
-        return res.render("shoppingCart")
+    shoppingCart: async (req, res) => {
+        const user = req.session.userLoggedIn
+        const userId = user.id
+        const shoppingCartProducts = await shoppingCartServices.getUserProducts(userId)
+
+        const finalProducts = await Promise.all(
+            shoppingCartProducts.map(async product => {
+                return await productsServices.findProductById(product.id_product)
+            })
+        )
+
+        return await res.render("shoppingCart", {finalProducts: finalProducts})
     },
 
     addShoppingCartProcess: (req, res) => {
-        
+        const user = req.session.userLoggedIn
+        const userId = user.id
+        const productId = req.params.productId
+
+        return shoppingCartServices.addProductToCart(userId, productId)
     }
 }
--- src/services/shoppingCartServices.js
+++ src/services/shoppingCartServices.js
@@ -1,7 +1,18 @@
-// const {Products} = require("../../database/models")
+const {Shopping_cart} = require("../../database/models")
 
 const shoppingCartServices = {
-    
+    getUserProducts: (userId) => {
+        return Shopping_cart.findAll({
+            where: {id_user: userId}
+        })
+    },
+
+    addProductToCart: (userId, productId) => {
+        return Shopping_cart.create({
+            id_user: userId,
+            id_product: productId
+        })
+    }
 }
 
 module.exports = shoppingCartServices
